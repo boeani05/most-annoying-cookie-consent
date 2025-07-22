ae7ad732e3809a555dc2cf3191678ec6df748bac
boeani05 <flyingdoge@gmx.at>
2025-07-22 11:13:50 +0200

    used formdata to use on popup

--- Scrim-s02ti1q/index.js
+++ Scrim-s02ti1q/index.js
@@ -13,14 +13,19 @@ modalCloseBtn.addEventListener("click", function() {
 
 consentForm.addEventListener("submit", function(e) {
     e.preventDefault() 
+
+    const consentFormData = new FormData(consentForm)
+    const name = consentFormData.get('fullName')
+
     modalText.innerHTML = '<div class="modal-inner-loading"><img src="images/loading.svg" class="loading"><p id="upload-text">Uploading your data to the dark web...</p></div>'
+    
     setTimeout(function() {
         document.getElementById('upload-text').innerText = 'Making the sale...'
     }, 1500)
 
     setTimeout(function() {
        document.getElementById('modal-inner').innerHTML =  `
-        <h2>Thanks you sucker!</h2>
+        <h2>Thanks <span class="modal-display-name">${name}</span>, you sucker!</h2>
         <p>We just sold the rights to your eternal soul.</p>
         <div class="idiot-gif">
             <img src="images/pirate.gif">
